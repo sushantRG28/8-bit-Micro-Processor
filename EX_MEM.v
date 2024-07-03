0b6a12693f9e0f2266825a06e41ca250502c7aab
l05er29 <163720414+l05er29@users.noreply.github.com>
2024-07-03 17:33:09 +0530

    Update EX_MEM.v

--- EX_MEM.v
+++ EX_MEM.v
@@ -1,13 +1,10 @@
 module EX_MEM(
   input clk,reset,
- // input [63:0] Adder_out  //adder output----> for the branch instructions to caluclte the next branch 
   input [7:0] Result_in_alu,
- // input Zero_in,
-  input [7:0] writedata_in, //2 bit mux2by1 output for load operation ig
+  input [7:0] writedata_in, //2 bit mux2by1 output for load operation 
   input [4:0] Rd_in, //IDEX output address of the destnation reg
   input memread_in,memtoreg_in,memwrite_in,regwrite_in, //IDEXX outputs control inputs 
   
-//  output reg zero,
   output reg [7:0] result_out_alu,
   output reg [7:0] writedata_out,
   output reg [4:0]rd,
@@ -32,7 +29,6 @@ module EX_MEM(
       else
         begin
          
-    //      zero <= Zero_in;
           result_out_alu <= Result_in_alu;
           writedata_out <= writedata_in;
           rd <= Rd_in;
