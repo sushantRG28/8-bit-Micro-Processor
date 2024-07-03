1daf2c2c9a99593f7825db21c1df29165aba9dfc
l05er29 <163720414+l05er29@users.noreply.github.com>
2024-07-03 17:24:47 +0530

    Update ALU_control.v

--- ALU_control.v
+++ ALU_control.v
@@ -1,10 +1,12 @@
 `timescale 1ns / 1ps
 
-module ALU_control(func,Op,ALUOp);
-    input [3:0] func;
+module ALU_control(funct3,funct7,Op,ALUOp);
+    input [2:0] funct3;
+    input [6:0] funct7;
     input [1:0] Op;
     output reg [3:0] ALUOp;
-
+    wire [3:0] func;
+    assign func = {funct7[5],funct3};
     
     always @(*) begin
         case (Op)
@@ -20,8 +22,37 @@ module ALU_control(func,Op,ALUOp);
           4'b0100: ALUOp = 4'b0100; //xor
           4'b0001: ALUOp = 4'b0101; //LLS
           4'b0101: ALUOp = 4'b0110; //LRS
+          4'b0011: ALUOp = 4'b0111; //SLT
+            default: ALUOp = 4'b1111; // Default 
+         endcase
+         case ({funct7,funct3})
+         10'b0000001000: ALUOp = 4'b1000;//Multiply lsb
+         10'b0000001011: ALUOp = 4'b1001;//Multiply hsb
+         10'b0000001101: ALUOp = 4'b1010;//div unsign 
+         10'b0000001111: ALUOp = 4'b1011;//rem unsign
          default: ALUOp = 4'b1111; // Default 
          endcase
+         end
+         2'b11:
+         begin
+         case (funct3)
+          3'b000: ALUOp = 4'b0000; //addni
+          3'b111: ALUOp = 4'b0010; //andi
+          3'b110: ALUOp = 4'b0011;//ori
+          3'b100: ALUOp = 4'b0100; //xori
+          3'b001: ALUOp = 4'b0101; //LLSi dbt
+          3'b101: ALUOp = 4'b0110; //LRSi dbt
+          3'b011: ALUOp = 4'b0111; //SLTi
+            default: ALUOp = 4'b1111; // Default 
+         
+         //that can be added
+         // arithmetic right shift
+         //
+         endcase
+         
+         
+         
+         
          end
         default: ALUOp = 4'b1111; // Default 
        endcase
@@ -30,3 +61,4 @@ module ALU_control(func,Op,ALUOp);
     
     
 endmodule
+
