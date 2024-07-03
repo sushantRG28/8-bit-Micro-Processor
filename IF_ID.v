a5c97e9d17982924863c4dff1a0771e3ac2d2492
l05er29 <163720414+l05er29@users.noreply.github.com>
2024-07-03 17:36:08 +0530

    Update IF_ID.v

--- IF_ID.v
+++ IF_ID.v
@@ -2,9 +2,6 @@ module IF_ID(input clk,
              input [7:0] pc_out, 
              input [31:0] instruction,
              input reset,
-//             input IFIDwrite,
-             //input flush,
-            // output reg [7:0] A_out,
              output reg [31:0] instruction_out
              );
              
@@ -12,12 +9,10 @@ always @(posedge clk)
   begin
     if(reset==1'b1)
     begin
-     // A_out=8'b0;
       instruction_out=32'b0;
    end
     else 
     begin
-     // A_out=pc_out;
       instruction_out<=instruction;
     end
   end
