63b57e033a7776be54f91e58af40c509cc48c328
Sushant Reddy G <135189884+sushantRG28@users.noreply.github.com>
2024-07-23 16:38:04 +0530

    Update imm_data.v

--- imm_data.v
+++ imm_data.v
@@ -10,21 +10,19 @@ module imm_data(
       case (instruction[6:5])
         2'b00: // I-type/Load
           begin
-            imm_data = instruction[31:24]; 
+            imm_data = instruction[27:20]; // This might only cover a subset of the actual immediate bits
           end
         2'b01: // S-type
           begin
             imm_data = {instruction[27:25], instruction[11:7]}; // Combining the two parts of the S-type immediate
           end
-        2'b10, 2'b11: // These are not be used, so we set it to default
+        2'b10, 2'b11: // These might not be used, setting to default
           begin
             imm_data = 8'b0; // Default case to handle unused values
           end
         default:
           begin
-            imm_data = 8'b0; 
+            imm_data = 8'b0; // Default case to handle unused values
           end
       endcase
     end  
-
-endmodule
