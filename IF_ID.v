9dae8527f849f07b839a0fb0884d845cd653b9e5
Sushant Reddy G <135189884+sushantRG28@users.noreply.github.com>
2024-07-30 23:35:45 +0530

    Update IF_ID.v

--- IF_ID.v
+++ IF_ID.v
@@ -1,19 +1,25 @@
 module IF_ID(input clk,
              input [7:0] pc_out, 
-             input [31:0] instruction,
+             input [31:0] instruction1,
+             input [31:0] instruction2,
              input reset,
-             output reg [31:0] instruction_out
+             input stall,
+             output reg [31:0] instruction_out1,
+             output reg [31:0] instruction_out2,
+             input flush
              );
              
 always @(posedge clk)
   begin
-    if(reset==1'b1)
+    if(reset==1'b1|| flush)
     begin
-      instruction_out=32'b0;
+      instruction_out1=32'b0;
+       instruction_out2 =32'b0;
    end
-    else 
+    else if (stall == 1'b0)
     begin
-      instruction_out<=instruction;
+      instruction_out1<=instruction1;
+      instruction_out2<=instruction2;
     end
   end
 endmodule
