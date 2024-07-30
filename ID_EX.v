8bd14a2b3cc10b47292ab1514cce96b796b8ba9f
Sushant Reddy G <135189884+sushantRG28@users.noreply.github.com>
2024-07-30 23:34:58 +0530

    Update ID_EX.v

--- ID_EX.v
+++ ID_EX.v
@@ -1,20 +1,42 @@
 module ID_EX(
 input clk,reset,
-input memwrite_in,memread_in,memtoreg_in,Alusrc_in,regwrite_in,//from CU
-input [1:0] Aluop_in,// from CU
-input [4:0] rd_in,
-input [7:0] readdata1_in,readdata2_in,//from regfile
-input [7:0] imm_data_in,//imm data extractor
-input  [2:0] func_in3,
-input  [6:0] func_in7,
-output  reg memwrite,memread,memtoreg,Alusrc,regwrite,
-output reg [1:0] Aluop,
-output  reg [4:0] rd,
-output  reg [7:0] readdata1,readdata2,
-output  reg [7:0] imm_data,
-output reg  [2:0] func_3,
-output reg  [6:0] func_7
+input memwrite_in1,memread_in1,memtoreg_in1,Alusrc_in1,regwrite_in1,//from CU
+input [1:0] Aluop_in1,// from CU
+input memwrite_in2,memread_in2,memtoreg_in2,Alusrc_in2,regwrite_in2,//from CU
+input [1:0] Aluop_in2,
+input [4:0] rd_in_1,
+input [4:0] rd_in_2,
+input [7:0] readdata1_in_1,readdata2_in_1,//from regfile
+input [7:0] readdata1_in_2,readdata2_in_2,
+input [7:0] imm_data_in_1,//imm data extractor
+input [7:0] imm_data_in_2,
+input  [2:0] func_in3_1,
+input  [6:0] func_in7_1,
+input  [2:0] func_in3_2,
+input  [6:0] func_in7_2,
+input [4:0] rs1_in_1,
+input [4:0] rs2_in_1,
+input [4:0] rs1_in_2,
+input [4:0] rs2_in_2,
 
+output  reg memwrite1,memread1,memtoreg1,Alusrc1,regwrite1,
+output reg [1:0] Aluop1,
+output  reg memwrite2,memread2,memtoreg2,Alusrc2,regwrite2,
+output reg [1:0] Aluop2,
+output  reg [4:0] rd_1,
+output  reg [4:0] rd_2,
+output  reg [7:0] readdata1_1,readdata2_1,
+output  reg [7:0] readdata1_2,readdata2_2,
+output  reg [7:0] imm_data_1,
+output reg  [2:0] func_3_1,
+output reg  [6:0] func_7_1,
+output  reg [7:0] imm_data_2,
+output reg  [2:0] func_3_2,
+output reg  [6:0] func_7_2,
+output reg [4:0] rs1_out_1,
+output reg [4:0] rs2_out_1,
+output reg [4:0] rs1_out_2,
+output reg [4:0] rs2_out_2
 
 
     );
@@ -22,35 +44,68 @@ output reg  [6:0] func_7
     begin
     if(reset==1)
     begin
-          rd <= 5'b0;
-          imm_data <= 8'b0;
-          readdata1 <= 8'b0;
-          readdata2  <= 8'b0;
+          rd_1 <= 5'b0;
+           rd_2 <= 5'b0;
+          imm_data_1 <= 8'b0;
+           imm_data_2 <= 8'b0;
+          readdata1_1 <= 8'b0;
+          readdata2_1  <= 8'b0;
+          readdata1_2 <= 8'b0;
+          readdata2_2  <= 8'b0;
        
-        func_3 <= 3'b0;
-          func_7 <= 7'b0;
-          memread <= 1'b0;
-          memtoreg <=1'b0;
-          memwrite <= 1'b0;
-          regwrite <= 1'b0;
-          Alusrc <= 1'b0;
-          Aluop <= 2'b0;
+        func_3_1 <= 3'b0;
+          func_7_1 <= 7'b0;
+           func_3_2 <= 3'b0;
+          func_7_2 <= 7'b0;
+          memread1 <= 1'b0;
+          memtoreg1 <=1'b0;
+          memwrite1 <= 1'b0;
+          regwrite1 <= 1'b0;
+          Alusrc1 <= 1'b0;
+          Aluop1 <= 2'b0;
+              memread2 <= 1'b0;
+          memtoreg2 <=1'b0;
+          memwrite2 <= 1'b0;
+          regwrite2 <= 1'b0;
+          Alusrc2 <= 1'b0;
+          Aluop2 <= 2'b0;
+          rs1_out_1 <= 5'b0;
+          rs2_out_1 <= 5'b0; 
+          rs1_out_2 <= 5'b0;
+          rs2_out_2 <= 5'b0;
           end
        
  else
         begin
-          rd <= rd_in;
-          imm_data <= imm_data_in;
-          readdata1 <= readdata1_in;
-          readdata2 <= readdata2_in;
-          func_3 <= func_in3;
-          func_7 <= func_in7;
-          memread <= memread_in;
-         memtoreg <= memtoreg_in;
-          memwrite <= memwrite_in;
-         regwrite <= regwrite_in;
-          Alusrc <= Alusrc_in;
-          Aluop <= Aluop_in;
+          rd_1 <= rd_in_1;
+          rd_2 <= rd_in_2;
+          imm_data_1 <= imm_data_in_1;
+           imm_data_2 <= imm_data_in_2;
+          readdata1_1 <= readdata1_in_1;
+          readdata2_1 <= readdata2_in_1;
+             readdata1_2 <= readdata1_in_2;
+          readdata2_2 <= readdata2_in_2;
+          func_3_1 <= func_in3_1;
+          func_7_1 <= func_in7_1;
+          func_3_2 <= func_in3_2;
+          func_7_2 <= func_in7_2;
+          memread1 <= memread_in1;
+         memtoreg1 <= memtoreg_in1;
+          memwrite1 <= memwrite_in1;
+         regwrite1 <= regwrite_in1;
+          Alusrc1 <= Alusrc_in1;
+          Aluop1 <= Aluop_in1;
+           memread2 <= memread_in2;
+         memtoreg2 <= memtoreg_in2;
+          memwrite2 <= memwrite_in2;
+         regwrite2 <= regwrite_in2;
+          Alusrc2 <= Alusrc_in2;
+          Aluop2 <= Aluop_in2;
+           
+          rs1_out_1 <= rs1_in_1;
+          rs2_out_1 <= rs2_in_1;
+          rs1_out_2 <= rs1_in_2;
+          rs2_out_2 <= rs2_in_2;
           
         end
    end
