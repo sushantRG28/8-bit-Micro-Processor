70a5b76a9f34de1d382f8aef7c7c4d83e3a9f94c
Sushant Reddy G <135189884+sushantRG28@users.noreply.github.com>
2024-07-30 23:40:40 +0530

    Update control_unit.v

--- control_unit.v
+++ control_unit.v
@@ -1,63 +1,168 @@
 `timescale 1ns / 1ps
 
 module control_unit(
-  input [6:0] opcode, 
-  output reg memread,
-  output reg memtoreg,
-  output reg memwrite,
-  output reg aluSrc,
-  output reg regwrite,
-  output reg [1:0] Aluop);
+  input [6:0] opcode1,
+  input [6:0] opcode2,
+  input stall, 
+  output reg memread1,
+  output reg memtoreg1,
+  output reg memwrite1,
+  output reg aluSrc1,
+  output reg regwrite1,
+  output reg [1:0] Aluop1,
+  output reg memread2,
+  output reg memtoreg2,
+  output reg memwrite2,
+  output reg aluSrc2,
+  output reg regwrite2,
+  output reg [1:0] Aluop2,
+  output reg branch1,
+  output reg branch2
+);
 
-       always @(*) begin
-    if (opcode == 7'b0000011) // Load instruction
+  always @(*) begin
+    // Control signals for first opcode
+    if (opcode1 == 7'b0000011) // Load instruction
     begin
-      memread = 1'b1;
-      memtoreg = 1'b1;
-      regwrite = 1'b1;
-      aluSrc = 1'b1;
-      memwrite = 1'b0;
-      Aluop = 2'b00;
+      memread1 = 1'b1;
+      memtoreg1 = 1'b1;
+      regwrite1 = 1'b1;
+      aluSrc1 = 1'b1;
+      memwrite1 = 1'b0;
+      Aluop1 = 2'b00;
+      branch1=1'b0;
     end
-    else if (opcode == 7'b0100011) // Store instruction
+    else if (opcode1 == 7'b0100011) // Store instruction
     begin
-      memread = 1'b0;
-      memtoreg = 1'bx;
-      regwrite = 1'b0;
-      aluSrc = 1'b1;
-      memwrite = 1'b1;
-      Aluop = 2'b00;
+      memread1 = 1'b0;
+      memtoreg1 = 1'bx;
+      regwrite1 = 1'b0;
+      aluSrc1 = 1'b1;
+      memwrite1 = 1'b1;
+      Aluop1 = 2'b00;
+      branch1=1'b0;
     end
-    else if (opcode == 7'b0110011) // R-Type Arithmetic instruction
+    else if (opcode1 == 7'b0110011) // R-Type Arithmetic instruction
     begin
-      memread = 1'b0;
-      memtoreg = 1'b0;
-      regwrite = 1'b1;
-      aluSrc = 1'b0;
-      memwrite = 1'b0;
-      Aluop = 2'b10;
+      memread1 = 1'b0;
+      memtoreg1 = 1'b0;
+      regwrite1 = 1'b1;
+      aluSrc1 = 1'b0;
+      memwrite1 = 1'b0;
+      Aluop1 = 2'b10;
+      branch1=1'b0;
     end
-    else if (opcode == 7'b0010011) // I-Type Arithmetic Instruction
+    else if (opcode1 == 7'b0010011) // I-Type Arithmetic Instruction
     begin
-      memread = 1'b0;
-      memtoreg = 1'b0;
-      regwrite = 1'b1;
-      aluSrc = 1'b1;
-      memwrite = 1'b0;
-      Aluop = 2'b11;
+      memread1 = 1'b0;
+      memtoreg1 = 1'b0;
+      regwrite1 = 1'b1;
+      aluSrc1 = 1'b1;
+      memwrite1 = 1'b0;
+      Aluop1 = 2'b11;
+      branch1=1'b0;      
+    end
+    else if (opcode1 == 7'b1100011) // I-Type Arithmetic Instruction
+    begin
+      memread1 = 1'b0;
+      memtoreg1 = 1'bx;
+      regwrite1 = 1'b0;
+      aluSrc1 = 1'b0;
+      memwrite1 = 1'b0;
+      Aluop1 = 2'b01;
+      branch1=1'b1;
+      end
+    else // default case
+    begin
+      memread1 = 1'b0;
+      memtoreg1 = 1'b0;
+      regwrite1 = 1'b0;
+      aluSrc1 = 1'b0;
+      memwrite1 = 1'b0;
+      Aluop1 = 2'b00;
+      branch1=1'b0;
+    end
+    
+
+    // Control signals for second opcode
+    if (opcode2 == 7'b0000011) // Load instruction
+    begin
+      memread2 = 1'b1;
+      memtoreg2 = 1'b1;
+      regwrite2 = 1'b1;
+      aluSrc2 = 1'b1;
+      memwrite2 = 1'b0;
+      Aluop2 = 2'b00;
+      branch2=1'b0;
+    end
+    else if (opcode2 == 7'b0100011) // Store instruction
+    begin
+      memread2 = 1'b0;
+      memtoreg2 = 1'bx;
+      regwrite2 = 1'b0;
+      aluSrc2 = 1'b1;
+      memwrite2 = 1'b1;
+      Aluop2 = 2'b00;
+      branch2=1'b0;
+    end
+     else if (opcode2 == 7'b1100011) // B-Type  instruction
+    begin
+      memread2 = 1'b0;
+      memtoreg2 = 1'bx;
+      regwrite2 = 1'b0;
+      aluSrc2 = 1'b0;
+      memwrite2 = 1'b0;
+      Aluop2 = 2'b01;
+      branch2=1'b1;
+    end
+    else if (opcode2 == 7'b0110011) // R-Type Arithmetic instruction
+    begin
+      memread2 = 1'b0;
+      memtoreg2 = 1'b0;
+      regwrite2 = 1'b1;
+      aluSrc2 = 1'b0;
+      memwrite2 = 1'b0;
+      Aluop2 = 2'b10;
+      branch2=1'b0;
+    end
+    else if (opcode2 == 7'b0010011) // I-Type Arithmetic Instruction
+    begin
+      memread2 = 1'b0;
+      memtoreg2 = 1'b0;
+      regwrite2 = 1'b1;
+      aluSrc2 = 1'b1;
+      memwrite2 = 1'b0;
+      Aluop2 = 2'b11;
+      branch2=1'b0;
     end
     else // default case
     begin
-      memread = 1'b0;
-      memtoreg = 1'b0;
-      regwrite = 1'b0;
-      aluSrc = 1'b0;
-      memwrite = 1'b0;
-      Aluop = 2'b00;
+      memread2 = 1'b0;
+      memtoreg2 = 1'b0;
+      regwrite2 = 1'b0;
+      aluSrc2 = 1'b0;
+      memwrite2 = 1'b0;
+      Aluop2 = 2'b00;
+      branch2=1'b0;
+    end
+
+    // Handle stall signal
+    if (stall == 1'b1) begin
+      memread1 = 1'b0;
+      memtoreg1 = 1'b0;
+      regwrite1 = 1'b0;
+      aluSrc1 = 1'b0;
+      memwrite1 = 1'b0;
+      Aluop1 = 2'b00;
+      branch2=1'b0;
+
+      memread2 = 1'b0;
+      memtoreg2 = 1'b0;
+      regwrite2 = 1'b0;
+      aluSrc2 = 1'b0;
+      memwrite2 = 1'b0;
+      Aluop2 = 2'b00;
+      branch2=1'b0;
     end
   end
-  
- 
-
-
 endmodule
